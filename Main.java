0bf2cbee993dd30f7ecbf28ab4f161f4dca09999
saluhera2023 <saluhera2023@gmail.com>
2026-03-10 10:39:48 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
-public class Main
-{
-    public static void main(String[] args) {
-        mostrarMensaje();
+class Personaje {
+    String nombre;
+    int vida;
+    int ataque;
+
+    public Personaje(String nombre, int vida, int ataque) {
+        this.nombre = nombre;
+        this.vida = vida;
+        this.ataque = ataque;
     }
 
-    public static void mostrarMensaje() {
-        System.out.println("hola, luis y luca");
-    
+    public void atacar(Personaje enemigo) {
+        enemigo.vida -= this.ataque;
+        System.out.println(nombre + " atacó a " + enemigo.nombre + " y le quitó " + ataque + " de vida.");
+    }
+
+    public void mostrarEstado() {
+        System.out.println(nombre + " tiene " + vida + " de vida.");
+    }
 }
-
-
-
