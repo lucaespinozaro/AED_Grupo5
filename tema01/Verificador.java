1f01d28bbf74e21fb4b19d4158afe15ad75beeda
saluhera2023 <saluhera2023@gmail.com>
2026-03-12 10:42:05 -0500

    Update Verificador.java

--- tema01/Verificador.java
+++ tema01/Verificador.java
@@ -8,3 +8,15 @@ public static boolean esSobrePos(Rectangulo r1, Rectangulo r2) {
             return false;
         }
     }
+        
+public static boolean esJunto(Rectangulo r1, Rectangulo r2) {
+        if (esSobrePos(r1, r2)) return false;       
+        boolean tcX = r1.getEsquina2().getX() == r2.getEsquina1().getX() || r1.getEsquina1().getX() == r2.getEsquina2().getX();
+        boolean tcY = r1.getEsquina2().getY() == r2.getEsquina1().getY() || r1.getEsquina1().getY() == r2.getEsquina2().getY();
+        return tcX || tcY;
+    }
+
+    public static boolean esDisjunto(Rectangulo r1, Rectangulo r2) {
+        return !esSobrePos(r1, r2) && !esJunto(r1, r2);
+    }
+}
