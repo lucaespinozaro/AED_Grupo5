822272b92da41830598a9a98e97b6a7e4459c20c
lucaespinozaro <luca.espinoza@estudiante.ucsm.edu.pe>
2026-03-12 10:14:15 -0500

    Create Rectangulo.java

---
+++ tema01/Rectangulo.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Rectangulo{
+  private Coordenada esquina1;
+  private Coordenada esquina2;
+  // Constructor
+  public Rectangulo(Coordenada c1, Coordenada c2) {
+    setEsquina1(c1);
+    setEsquina2(c2);
+  }
+  public void setEsquina1 (Coordenada coo) {
+    // fill in the code here
+  }
+  public void setEsquina2 (Coordenada coo) {
+    // fill in the code here
+  }
+  public Coordenada getEsquina1 () {
+    // fill in the code here
+  }
+  public Coordenada getEsquina2 () {
+    // fill in the code here
+  }
+  public String toString() {
+    // fill in the code here
+  }
+}
