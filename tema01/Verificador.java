4cb8ebed20d451997b4c71daffce6b00940837b8
saluhera2023 <saluhera2023@gmail.com>
2026-03-12 10:36:14 -0500

    Update Verificador.java

--- tema01/Verificador.java
+++ tema01/Verificador.java
@@ -1,7 +1,7 @@
 public class Verificador {
 public static boolean esSobrePos(Rectangulo r1, Rectangulo r2) {
         boolean shrz = (r1.getEsquina2().getX() < r2.getEsquina2().getX()) || (r1.getEsquina1().getX() > r2.getEsquina2().getX());
-        boolean sver = (r1.getEsquina2().getY() < r2.getEsquina1().getY()) || (r1.getEsquina1().getY() >double r2_maxY = r2.getEsquina2().getY();
+        boolean sver = (r1.getEsquina2().getY() < r2.getEsquina1().getY()) || (r1.getEsquina1().getY() > r2.getEsquina2().getY();
         if (!shr && !sver) {
             return true;
         } else {
