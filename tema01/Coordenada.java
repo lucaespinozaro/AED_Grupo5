ca7ad76a87735108b36f15e286835f174039f931
lucaespinozaro <luca.espinoza@estudiante.ucsm.edu.pe>
2026-03-12 10:11:32 -0500

    Update Coordenada.java

--- tema01/Coordenada.java
+++ tema01/Coordenada.java
@@ -1,1 +1,42 @@
-
+import java.util.*;
+public class Coordenada{
+private double x;
+private double y;
+//Constructor, initialize x, y attributes to zero
+public Coordenada( ){
+  }
+//Constructor
+public Coordenada(double x, double y ){
+// fill in the code here
+}
+//Constructor
+public Coordenada(Coordenada c ){
+// fill in the code here
+}
+//métodos setter
+void setX(double x) {
+// fill in the code here
+}
+void setY(double y){
+// fill in the code here
+}
+//métodos getter
+double getX(){
+// fill in the code here
+}
+double getY(){
+// fill in the code here
+}
+//método de instancia que calcula la distancia euclideana
+double distancia(Coordenada c){
+// fill in the code here
+}
+//método de clase que calcula la distancia euclideana
+static double distancia(Coordenada c1, Coordenada c2){
+// fill in the code here
+}
+//método que devuelve los valores de una coordenada en determinado formato
+String toString(){
+// fill in the code here
+}
+}
