171a9da5a4476f70e5bf4c88246bc4c165375eea
lucaespinozaro <luca.espinoza@estudiante.ucsm.edu.pe>
2026-03-12 10:33:24 -0500

    Update Main.java

--- tema01/Main.java
+++ tema01/Main.java
@@ -1,5 +1,9 @@
 public class Main{
   public static void main(){
+    Coordenada c1 = new Coordenada(10, 20);
+    Coordenada c2 = new Coordenanda(5, 10);
+
+    Rectangulo r1 = new Rectangulo(c1, c2);
     
   }
 }
